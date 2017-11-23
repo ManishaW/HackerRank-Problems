f35a01e50a039f004b93d499c31686b53825ca2e
Manisha W <manishaw@espial.com>
2017-11-23 12:46:57 -0500

    formatting

--- between-two-sets.java
+++ between-two-sets.java
@@ -12,65 +12,72 @@ public class Solution {
         int n = in.nextInt();
         int m = in.nextInt();
         int[] a = new int[n];
+        
         for(int a_i=0; a_i < n; a_i++){
             a[a_i] = in.nextInt();
         }
+
         int[] b = new int[m];
+        
         for(int b_i=0; b_i < m; b_i++){
             b[b_i] = in.nextInt();
         }
         
         
         int rangeStart=lcm(a);
-       int rangeEnd=gcd(b);
+        int rangeEnd=gcd(b);
         int counter=1;
         int totalx=0;
         int product=0;
 
-       while ((product<=rangeEnd)){
-          product= (rangeStart*counter);
-        if (rangeEnd%product==0){
-            totalx++;
-             }
-        counter++; 
-         }
+        while ((product<=rangeEnd)){
+            product= (rangeStart*counter);
+            
+            if (rangeEnd%product==0){
+                totalx++;
+            }
+
+            counter++; 
+        }                                      
         
         System.out.println(totalx);
 
-         }
+    }
         //find lcm in A
         private static int lcm(int[] input){
         int result = input[0];
+        
         for(int i = 1; i < input.length; i++){
              result = lcm2(result, input[i]);
         }
+
         return result;
         }
     
-    private static int lcm2(int a, int b)
-{
-    return a * (b / gcd2(a, b));
-}
+    private static int lcm2(int a, int b){
+        return a * (b / gcd2(a, b));
+    }
 
         
-        //find gcf in B
+    //find gcf in B
 
-         private static int gcd(int[] input){
-        int result = input[0];
-        for(int i = 1; i < input.length; i++){
-            result = gcd2(result, input[i]);
-        } 
-        return result;
-        }  
+    private static int gcd(int[] input){
+    int result = input[0];
+
+    for(int i = 1; i < input.length; i++){
+        result = gcd2(result, input[i]);
+    } 
     
-    private static int gcd2(int a, int b)
-{
-    while (b > 0)
-    {
-        int temp = b;
-        b = a % b; // % is remainder
-        a = temp;
+    return result;
+    }  
+    
+    private static int gcd2(int a, int b){
+        while (b > 0)
+        {
+            int temp = b;
+            b = a % b; // % is remainder
+            a = temp;
+        }
+        return a;
     }
-    return a;
-}
 }
